5879f248fd825a9060f57ca5fecffe947baa1035
Eric Peterson <ericp@math.berkeley.edu>
2013-08-01 12:48:19 -0400

    incremental commit because i broke the toolbar

---
+++ EXTGeneratorInspectorViewController.h
@@ -0,0 +1,13 @@
+//
+//  EXTGeneratorInspectorViewController.h
+//  Ext Chart
+//
+//  Created by Eric Peterson on 8/1/13.
+//  Copyright (c) 2013 Harvard University. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface EXTGeneratorInspectorViewController : NSViewController
+
+@end
---
+++ EXTGeneratorInspectorViewController.m
@@ -0,0 +1,31 @@
+//
+//  EXTGeneratorInspectorViewController.m
+//  Ext Chart
+//
+//  Created by Eric Peterson on 8/1/13.
+//  Copyright (c) 2013 Harvard University. All rights reserved.
+//
+
+#import "EXTGeneratorInspectorViewController.h"
+
+@interface EXTGeneratorInspectorViewController ()
+
+@end
+
+@implementation EXTGeneratorInspectorViewController
+
+- (id)init {
+    return [super initWithNibName:@"EXTGeneratorInspectorViewController" bundle:nil];
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
