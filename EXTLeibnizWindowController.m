c8a549fd6ab7d21e274ba15227fc3707c6b48831
Bavarious <bavarious@icloud.com>
2013-09-23 00:13:01 -0300

    Removes unmodified template methods from EXTLeibnizWindowController.

--- EXTLeibnizWindowController.m
+++ EXTLeibnizWindowController.m
@@ -24,23 +24,6 @@
 
 @implementation EXTLeibnizWindowController
 
-- (id)initWithWindow:(NSWindow *)window
-{
-    self = [super initWithWindow:window];
-    if (self) {
-        // Initialization code here.
-    }
-    
-    return self;
-}
-
-- (void)windowDidLoad
-{
-    [super windowDidLoad];
-    
-    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
-}
-
 - (void)showWindow:(id)sender {
     [self.tableView deselectAll:nil];
     self.list = [NSMutableArray array];
