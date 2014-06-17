a11b7ac0c066d8016a32f332473186d4c5929963
Eric Peterson <ericp@math.berkeley.edu>
2014-06-17 12:04:55 -0400

    hook up the stepper and text field

--- EXTNakamuraWindowController.m
+++ EXTNakamuraWindowController.m
@@ -12,14 +12,19 @@
 #import "EXTChartViewController.h"
 #import "EXTDocument.h"
 
-@interface EXTNakamuraWindowController ()
+@interface EXTNakamuraWindowController () <NSTextFieldDelegate>
 
 @property IBOutlet EXTMatrixEditor *editor;
 @property IBOutlet NSTextField *field;
 @property IBOutlet NSStepper *stepper;
 @property IBOutlet NSButton *OKbutton;
 
+@property (assign) int degree;
+
 @end
+
+
+
 
 @implementation EXTNakamuraWindowController
 {
@@ -36,11 +41,24 @@
     return;
 }
 
+-(IBAction)changeDegreeValue:(id)sender {
+    self.degree = [sender intValue];
+    [self.field setIntegerValue:self.degree];
+    [self.stepper setIntegerValue:self.degree];
+}
+
+-(void)controlTextDidChange:(NSNotification *)obj {
+    int value = [((NSTextView*)obj.userInfo[@"NSFieldEditor"]).textStorage.string integerValue];
+    
+    [self changeDegreeValue:@(value)];
+}
+
 - (id)initWithWindow:(NSWindow *)window
 {
     self = [super initWithWindow:window];
     if (self) {
         self.editor.representedObject = nil;
+        self.degree = 0;
     }
     return self;
 }
@@ -51,6 +69,8 @@
     
     // Implement this method to handle any initialization after your window
     // controller's window has been loaded from its nib file.
+    [self.stepper bind:@"integerValue" toObject:self withKeyPath:@"degree" options:nil];
+    [self.field bind:@"integerValue" toObject:self withKeyPath:@"degree" options:nil];
 }
 
 -(void)mouseDownAtGridLocation:(EXTIntPoint)gridLocation {
